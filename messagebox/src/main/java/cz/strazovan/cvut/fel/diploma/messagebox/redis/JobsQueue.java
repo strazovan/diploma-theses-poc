724dfe36e57ec62f81c75ff0bee59d7292b9550f
David Stražovan <strazdav@fel.cvut.cz>
2022-05-08 16:12:34 +0200

    metrics for messagebox

--- messagebox/src/main/java/cz/strazovan/cvut/fel/diploma/messagebox/redis/JobsQueue.java
+++ messagebox/src/main/java/cz/strazovan/cvut/fel/diploma/messagebox/redis/JobsQueue.java
@@ -1,22 +1,52 @@
 package cz.strazovan.cvut.fel.diploma.messagebox.redis;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
-public class JobsQueue {
+public class JobsQueue implements InitializingBean {
 
     @Resource(name = "redisTemplate")
     private ListOperations<String, String> listOps;
 
+    @Value("${job.type}")
+    private String jobTypeName;
+
+    private final MeterRegistry meterRegistry;
+
+    private final AtomicLong queueSize = new AtomicLong(0);
+
+    public JobsQueue(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+
     public void addJobToLocalQueue(String job) {
         this.listOps.rightPush("jobs", job);
+        this.queueSize.set(Objects.requireNonNull(this.listOps.size("jobs")));
     }
 
     public String getJobForAgent() {
-        return this.listOps.leftPop("jobs");
+        final String jobs = this.listOps.leftPop("jobs");
+        this.queueSize.set(Objects.requireNonNull(this.listOps.size("jobs")));
+        return jobs;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.queueSize.set(this.listOps.size("jobs"));
+        final String gaugeName = this.jobTypeName + ".queue.size";
+        this.meterRegistry.gauge(gaugeName, this.queueSize);
     }
 }
